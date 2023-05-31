299fb262d7edac4679a12b9d8da6631f7508fcf6
Exponential-Workload <cc@aaathats3as.com>
2023-05-31 12:42:10 +0200

    fix: disclose package sources

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-pkg -t node18-win-x64,node18-linux-x64,node18-macos-x64 -o out/index index.js --config package.json --public --no-bytecode
+pkg -t node18-win-x64,node18-linux-x64,node18-macos-x64 -o out/index index.js --config package.json --public --no-bytecode --public-packages "*"
