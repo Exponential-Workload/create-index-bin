22e5dda6c0786755799f18e696468764a172973c
Exponential-Workload <cc@aaathats3as.com>
2023-05-31 13:11:22 +0200

    fix: no boxen

--- index.js
+++ index.js
@@ -2,4 +2,5 @@ if (process.argv.length < 3) {
   process.argv.push('build')
   console.log(`No command specified, defaulting to 'build'`)
 }
+globalThis._no_boxen_createindex = 1
 require('@3xpo/create-index/dist/index.js')
