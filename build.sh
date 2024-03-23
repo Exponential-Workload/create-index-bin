a68a29f359826573f5a66c7054c965e543308dc9
Exponential-Workload <cc@aaathats3as.com>
2024-03-23 11:13:28 +0100

    fix: new stuff

--- build.sh
+++ build.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
-pkg -t node18-win-x64,node18-linux-x64,node18-macos-x64 -o out/index index.js --config package.json --public --no-bytecode --public-packages "*"
+set -eax
+esbuild --bundle --platform=node --target=node18 --outfile=out/create.js --format=cjs --packages=external index.js
+pkg -t node18-win-x64,node18-linux-x64,node18-macos-x64 -o out/index out/create.js --config package.json --public --no-bytecode --public-packages "*"  > pkg.log 2> pkg.log
+rm out/create.js
--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 if (process.argv.length < 3) {
-  process.argv.push('build')
-  console.log(`No command specified, defaulting to 'build'`)
+  process.argv.push('build');
+  console.log(`No command specified, defaulting to 'build'`);
 }
-globalThis._no_boxen_createindex = 1
-require('@3xpo/create-index/dist/index.js')
+globalThis._no_boxen_createindex = 1;
+require('@3xpo/create-index/dist/index.cjs');
